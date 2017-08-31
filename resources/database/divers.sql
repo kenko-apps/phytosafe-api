99954a8dfcd970bd9d36ce01011787a970ef342b
sdc <android212@gmail.com>
2017-08-31 21:24:18 +0200

    scripts de reconstructon de la base

--- resources/database/divers.sql
+++ resources/database/divers.sql
@@ -1,8 +1,14 @@
 
 --- donner tous les droits sur une base à un utilisateur
+GRANT ALL PRIVILEGES ON SCHEMA public TO s;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO s;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO s;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
   GRANT ALL PRIVILEGES ON TABLES TO s;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
   GRANT ALL PRIVILEGES ON SEQUENCES TO s;
+
+
+--- supprimer et recreer un schéma
+DROP schema public CASCADE;
+CREATE schema public;
