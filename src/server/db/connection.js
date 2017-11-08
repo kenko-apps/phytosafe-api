71ead7652b795f2301987d8575f82bdfc765c6cf
callio <cedricallio@yahoo.fr>
2017-11-08 15:16:20 +0100

    Mise à jour des requêtes

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -5,9 +5,7 @@ const pgp = require('pg-promise')(options);
 const cn = {
   host: '127.0.0.1', // 'localhost' is the default
   port: 5432, // 5432 is the default
-  password:'xxxxxx',
-  user:'postgres',
-  password:'xxxxxx'
+
 };
 
 const db = pgp(cn); // database instance
