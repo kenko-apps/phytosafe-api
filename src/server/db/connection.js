12657f60d87d41650d5a188693c179fb2cfe4415
callio <cedricallio@yahoo.fr>
2018-03-26 15:10:10 +0200

    Mise en production

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -3,7 +3,7 @@ const pgp = require('pg-promise')(options);
 
 // database connection details
 const cn = {
-  host: '51.255.38.12',//'127.0.0.1', // 'localhost' is the default
+  host: '127.0.0.1', //'51.255.38.12', // 'localhost' is the default
   port: 5432, // 5432 is the default
   password:'xxxxxx',
   password:'xxxxxx',
