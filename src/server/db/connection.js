dc171e5b15ccd120080b7259cceca9cba834b252
callio <cedricallio@yahoo.fr>
2018-03-30 23:00:07 +0200

    Modification indentation

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -5,9 +5,9 @@ const pgp = require('pg-promise')(options);
 const cn = {
   host: '127.0.0.1', // 'localhost' is the default
   port: 5432, // 5432 is the default
-  database: 'xxxxxx',
-  user:'xxxxxx',
-  password:'xxxxxxx'
+  database: 'xxxxxxx',
+  user:'xxxxxxx',
+  password:'xxxxxxxx'
 };
 
 const db = pgp(cn); // database instance
