4678227d8f23c19d0db257ecd84990461fdeb652
callio <cedricallio@yahoo.fr>
2018-03-30 21:56:31 +0200

    Purge du fichier connection.js

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -3,11 +3,11 @@ const pgp = require('pg-promise')(options);
 
 // database connection details
 const cn = {
-  host: '127.0.0.1', //'51.255.38.12', // 'localhost' is the default
+  host: '127.0.0.1', // 'localhost' is the default
   port: 5432, // 5432 is the default
-  password:'xxxxxx',
-  password:'xxxxxx',
-  password:'xxxxxx'
+  database: 'xxxxxx',
+  user:'xxxxxx',
+  password:'xxxxxxx'
 };
 
 const db = pgp(cn); // database instance
